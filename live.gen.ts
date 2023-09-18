ec93d9866b1e14efc92f5f94340f12d5285383ab
Renan Lopes Ferreira <relferreira@gmail.com>
2023-09-18 11:18:45 -0300

    New footer component

--- live.gen.ts
+++ live.gen.ts
@@ -4,11 +4,12 @@
 
 import * as $$$$0 from "./routes/styles.css.ts";
 import * as $$$$1 from "./routes/_app.tsx";
-import * as $$$$$$$$0 from "./sections/Hero.tsx";
-import * as $$$$$$$$1 from "./sections/Theme.tsx";
-import * as $$$$$$$$2 from "./sections/Footer.tsx";
-import * as $$$$$$$$3 from "./sections/SimpleTextList.tsx";
-import * as $$$$$$$$4 from "./sections/ParagraphList.tsx";
+import * as $$$$$$$$0 from "./sections/Footer2.tsx";
+import * as $$$$$$$$1 from "./sections/Hero.tsx";
+import * as $$$$$$$$2 from "./sections/Theme.tsx";
+import * as $$$$$$$$3 from "./sections/Footer.tsx";
+import * as $$$$$$$$4 from "./sections/SimpleTextList.tsx";
+import * as $$$$$$$$5 from "./sections/ParagraphList.tsx";
 import * as $live_workflows from "$live/routes/live/workflows/run.ts";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
@@ -162,11 +163,12 @@ const manifest = {
     "$live/sections/PageInclude.tsx": i2$$$$$2,
     "$live/sections/Slot.tsx": i2$$$$$3,
     "$live/sections/UseSlot.tsx": i2$$$$$4,
-    "deco-sites/scribo-teste/sections/Footer.tsx": $$$$$$$$2,
-    "deco-sites/scribo-teste/sections/Hero.tsx": $$$$$$$$0,
-    "deco-sites/scribo-teste/sections/ParagraphList.tsx": $$$$$$$$4,
-    "deco-sites/scribo-teste/sections/SimpleTextList.tsx": $$$$$$$$3,
-    "deco-sites/scribo-teste/sections/Theme.tsx": $$$$$$$$1,
+    "deco-sites/scribo-teste/sections/Footer.tsx": $$$$$$$$3,
+    "deco-sites/scribo-teste/sections/Footer2.tsx": $$$$$$$$0,
+    "deco-sites/scribo-teste/sections/Hero.tsx": $$$$$$$$1,
+    "deco-sites/scribo-teste/sections/ParagraphList.tsx": $$$$$$$$5,
+    "deco-sites/scribo-teste/sections/SimpleTextList.tsx": $$$$$$$$4,
+    "deco-sites/scribo-teste/sections/Theme.tsx": $$$$$$$$2,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$$5,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$$6,
     "deco-sites/std/sections/configNuvemShop.tsx": i2$$$$$7,
